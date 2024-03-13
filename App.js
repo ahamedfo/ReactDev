70d00d6c664a48e8c41ea99a297c197038a301c8
ahamed <ahamedfofana12@gmail.com>
2024-03-13 13:40:49 -0400

    Styling updates - goals

--- App.js
+++ App.js
@@ -2,14 +2,13 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <View style={styles.appContainer}>
-        <TextInput placeholder="Your course goal!" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
         <Button title="Add goal"/>
       </View>
-
-      <View>
-        <Text>List of goals...</Text>
+      <View style={styles.goalsContainer}>
+        <Text> List of goals...</Text>
       </View>
 
     </View>
@@ -21,6 +20,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50
+    flex:1,
+    paddingTop: 50,
+    paddingHorizontal: 16
+  },
+  inputContainer:{
+    flex:1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth:1,
+    borderBottomColor: '#cccccc'
+  },
+  textInput:{
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8
+  },
+  goalsContainer: {
+    flex: 5
   }
 });
