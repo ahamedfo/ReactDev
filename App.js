c2552e173d4c7711a438af5519cf1e23a0734c30
ahamed <ahamedfofana12@gmail.com>
2024-03-14 11:36:23 -0400

    App styles commit

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
+import {useState} from 'react'
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText)
+   };
+
+  function addGoalHandler() { 
+      console.log(enteredGoalText)
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button title="Add goal"/>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" onChangeText={goalInputHandler} />
+        <Button title="Add goal" onPress={addGoalHandler}/>
       </View>
       <View style={styles.goalsContainer}>
         <Text> List of goals...</Text>
@@ -20,20 +31,20 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    flex:1,
+    flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16
   },
-  inputContainer:{
-    flex:1,
+  inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     marginBottom: 24,
-    borderBottomWidth:1,
+    borderBottomWidth: 1,
     borderBottomColor: '#cccccc'
   },
-  textInput:{
+  textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
     width: '70%',
